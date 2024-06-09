729716680a89ccf348321d79e4adf642effb5b88
usamashakoor12 <136337480+usamashakoor12@users.noreply.github.com>
2024-06-09 18:29:01 +0500

    Personal Portfolio

---
+++ js/script.js
@@ -0,0 +1,7 @@
+// ===========   typing animation  ================= //
+var typed = new Typed(".typing", {
+  strings: ["", "Web Developer", "Software Developer", "React Developer"],
+  typeSpeed: 100,
+  BackSpeed: 60,
+  loop: true,
+});
